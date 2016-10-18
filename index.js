6453d553b588212dc7c831f22f96a1caf5d0687e
Dimitar Timofeev <Dimitar.Timofeev@Softwaregroup-bg.com>
2016-10-18 11:22:29 +0300

    feat: implement mock for spsp client routes

--- index.js
+++ index.js
@@ -91,6 +91,164 @@ server.route([
         failAction: directoryFailActionHandler
       }
     }
+  },
+  {
+    path: '/spspclient/query',
+    method: 'get',
+    handler: (request, reply) => {
+      var receiver = request.query.receiver.split('/').pop()
+      if (['alice', 'bob'].indexOf(receiver) === -1) {
+        return reply({
+          'id': 'Error',
+          'message': 'Error getting receiver details, receiver responded with: undefined getaddrinfo ENOTFOUND ' + receiver + ' ' + receiver + ':80',
+          'debug': {}
+        })
+      }
+      return reply({
+        'currencyCode': 'USD',
+        'imageUrl': 'http://mediaserver.com/demo/images/' + receiver + '-profile-pic.jpg',
+        'currencySymbol': '$',
+        'name': receiver + ' Taylor',
+        'type': 'payee',
+        'address': 'levelone.dfsp2.' + receiver
+      })
+    }
+  },
+  {
+    path: '/spspclient/quoteSourceAmount',
+    method: 'get',
+    handler: (request, reply) => {
+      var receiverUri = request.query.receiver
+      var sourceAmount = request.query.sourceAmount
+      var receiver = receiverUri.split('/').pop()
+
+      if (!sourceAmount) {
+        return reply({
+          'id': 'BadRequest',
+          'message': 'sourceAmount query string parameter is required'
+        })
+      }
+      if (!receiverUri || !receiver) {
+        return reply({
+          'error': {
+            'id': 'Bad request',
+            'message': 'Failed to process request for interopID=2b39b6ab-8a9f-4a8d-9257-9ca2d73c2561: Required query parameter receiver not specified'
+          },
+          'debug': {}
+        })
+      }
+      return reply({
+        'destinationAmount': sourceAmount * 0.975
+      })
+    }
+  },
+  {
+    path: '/spspclient/quoteDestinationAmount',
+    method: 'get',
+    handler: (request, reply) => {
+      var receiverUri = request.query.receiver
+      var destinationAmount = request.query.destinationAmount
+      var receiver = receiverUri.split('/').pop()
+
+      if (!destinationAmount) {
+        return reply({
+          'id': 'BadRequest',
+          'message': 'destinationAmount query string parameter is required'
+        })
+      }
+      if (!receiverUri || !receiver) {
+        return reply({
+          'error': {
+            'id': 'Bad request',
+            'message': 'Failed to process request for interopID=2b39b6ab-8a9f-4a8d-9257-9ca2d73c2561: Required query parameter receiver not specified'
+          },
+          'debug': {}
+        })
+      }
+      return reply({
+        'sourceAmount': destinationAmount * 1.025
+      })
+    }
+  },
+  {
+    path: '/spspclient/setup',
+    method: 'post',
+    handler: (request, reply) => {
+      var receiver = request.payload.receiver.split('/').pop()
+      if (['alice', 'bob'].indexOf(receiver) === -1) {
+        return reply({
+          'id': 'Error',
+          'message': 'Error getting receiver details, receiver responded with: 500 Internal Server Error',
+          'debug': {}
+        })
+      }
+      var date = new Date()
+      return reply({
+        'id': '9b5b6198-52ab-4c05-a875-72cf7448dc51',
+        'receiver': request.payload.receiver,
+        'sourceAmount': (request.payload.destinationAmount * 1.025).toString(),
+        'destinationAmount': request.payload.destinationAmount,
+        'address': 'levelone.dfsp2.' + receiver + '.9b5b6198-52ab-4c05-a875-72cf7448dc51',
+        'memo': request.payload.memo,
+        'expiresAt': date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDay() + 'T' + date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds() + '.199Z',
+        'condition': 'cc:0:3:qLvNojWtv8zQuhJ0XuTVJMl_OBUmFZFJumhvuDxSk44:32',
+        'sourceAccount': request.payload.sourceAccount
+      })
+    },
+    config: {
+      validate: {
+        payload: joi.object({
+          'receiver': joi.string().required(),
+          'sourceAccount': joi.string().required(),
+          'destinationAmount': joi.string().required(),
+          'memo': joi.string().required(),
+          'sourceIdentifier': joi.string().required()
+        }),
+        failAction: directoryFailActionHandler
+      }
+    }
+  },
+  {
+    path: '/spspclient/payments/{paymentId}',
+    method: 'put',
+    handler: (request, reply) => {
+      var receiver = request.payload.receiver.split('/').pop()
+      if (['alice', 'bob'].indexOf(receiver) === -1) {
+        return reply({
+          'id': 'Error',
+          'message': 'Error getting receiver details, receiver responded with: 500 Internal Server Error',
+          'debug': {}
+        })
+      }
+      return reply({
+        'id': request.payload.id,
+        'address': request.payload.address,
+        'destinationAmount': request.payload.destinationAmount,
+        'sourceAmount': request.payload.sourceAmount,
+        'sourceAccount': request.payload.sourceAccount,
+        'expiresAt': request.payload.expiresAt,
+        'additionalHeaders': 'asdf98zxcvlknannasdpfi09qwoijasdfk09xcv009as7zxcv',
+        'condition': request.payload.condition,
+        'fulfillment': 'cf:0:qUAo3BNo49adBtbYTab2L5jAWLpAhnrkNQamsMYjWvM',
+        'status': 'executed'
+      })
+    },
+    config: {
+      validate: {
+        payload: joi.object({
+          'id': joi.string().required(),
+          'receiver': joi.string().required(),
+          'sourceAmount': joi.string().required(),
+          'destinationAmount': joi.string().required(),
+          'address': joi.string().required(),
+          'memo': joi.string(),
+          'expiresAt': joi.string().required(),
+          'condition': joi.string().required(),
+          'sourceAccount': joi.string().required()
+        }),
+        failAction: directoryFailActionHandler
+      }
+    }
   }
 ])
 
