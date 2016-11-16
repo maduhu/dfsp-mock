a82f4ecd17388f45b6a1150566d6fd5753015970
stamen.peev <stamen.peev@softwaregroup-bg.com>
2016-11-16 17:35:45 +0200

    fix: add stop method

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const hapi = require('hapi')
 const joi = require('joi')
-
+/*eslint no-console:0 */
 const server = new hapi.Server()
 server.connection({ port: 8021 })
 
@@ -281,4 +281,11 @@ server.start((err) => {
   if (err) {
     throw err
   }
+})
+
+module.exports = Promise.resolve({
+ stop: function() {
+   server.stop()
+   return Promise.resolve()
+ }
 })
